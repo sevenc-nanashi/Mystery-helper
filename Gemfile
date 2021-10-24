d14ec52d863e3f902e68cf504ee40faa67b3b687
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-10-25 05:37:38 +0900

    Update: discorbをアップデート

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source "https://rubygems.org"
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
-gem "discorb", "~> 0.11.3", github: "discorb-lib/discorb"
+gem "discorb", "~> 0.12.2"
 gem "dotenv", "~> 2.7"
 
-gem "dispander.rb", "~> 0.3.1"
+gem "dispander.rb", "~> 0.3.2"
--- main.rb
+++ main.rb
@@ -13,16 +13,23 @@ end
 client.on :reaction_add do |reaction|
   next if reaction.emoji != Discorb::UnicodeEmoji["pushpin"]
 
+  # @type [Discorb::Message]
   message = reaction.fetch_message.wait
   next if message.pinned?
-
-  message.pin
+  unless [:default, :reply].include? message.type
+    sent_message = message.channel.post("→ #{reaction.from.mention}\n\nこのメッセージはピン留めできません。").wait
+    sleep 5
+    sent_message.delete!
+    next
+  end
+  message.pin.wait
 end
 
 client.on :reaction_remove do |reaction|
   next if reaction.emoji != Discorb::UnicodeEmoji["pushpin"]
 
   message = reaction.fetch_message(force: true).wait
+  next unless message.pinned?
 
   message.unpin
 end
